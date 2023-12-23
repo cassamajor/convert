83888ef9c7367328ccb92b3c3da5d95cc48a1c42
Steven Cassamajor <contributor@networkaxis.org>
2023-12-22 20:26:45 -0500

    Implement option constructor (aka functional options)

--- convert.go
+++ convert.go
@@ -6,16 +6,48 @@ import (
 	"strings"
 )
 
+type Printer struct {
+	Print func(w io.Writer, a ...any) (n int, err error)
+}
+
+type option func(*Printer)
+
+func WithPrinter(print func(w io.Writer, a ...any) (n int, err error)) option {
+	return func(p *Printer) {
+		p.Print = print
+	}
+}
+
+func NewPrinter(opts ...option) *Printer {
+	p := &Printer{Print: fmt.Fprint}
+
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	return p
+}
+
+func (p *Printer) String(w io.Writer) string {
+	b := new(strings.Builder)
+	p.Print(b, w)
+	return b.String()
+}
+
+func (p *Printer) Stringln(w io.Writer) string {
+	b := new(strings.Builder)
+	p.Print(b, w)
+	return b.String()
+}
+
 // String will write the content of an io.Writer to a string builder without a trailing newline.
 func String(w io.Writer) string {
-	b := new(strings.Builder)
-	fmt.Fprint(b, w)
-	return b.String()
+	p := NewPrinter()
+	return p.String(w)
 }
 
 // Stringln will write the content of an io.Writer to a string builder with a trailing newline.
 func Stringln(w io.Writer) string {
-	b := new(strings.Builder)
-	fmt.Fprintln(b, w)
-	return b.String()
+	p := NewPrinter(WithPrinter(fmt.Fprintln))
+	return p.String(w)
 }
