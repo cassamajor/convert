749b2aca765906d10728ad244464ed1fa243db6c
Steven Cassamajor <contributor@networkaxis.org>
2023-12-22 20:47:20 -0500

    Update tests to use internal methods, structs, and functions

--- convert_test.go
+++ convert_test.go
@@ -12,30 +12,32 @@ func TestConvert(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name      string
-		function  func(io.Writer) string
-		printType func(w io.Writer, a ...any) (n int, err error)
-		want      string
+		name     string
+		function func(io.Writer) string
+		printer  convert.Printer
+		want     string
 	}{
 		{
-			name:      "Content will not include a trailing newline.",
-			function:  convert.String,
-			printType: fmt.Fprint,
-			want:      "Hello, world",
+			name:     "Content will not include a trailing newline.",
+			function: convert.String,
+			printer:  convert.Printer{Print: fmt.Fprint},
+			want:     "Hello, world",
 		},
 		{
-			name:      "Content will include a trailing newline",
-			function:  convert.Stringln,
-			printType: fmt.Fprintln,
-			want:      "Hello, world\n\n",
+			name:     "Content will include a trailing newline",
+			function: convert.Stringln,
+			printer:  convert.Printer{Print: fmt.Fprintln},
+			want:     "Hello, world\n\n",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			//t.Parallel()
+			t.Parallel()
 
 			writer := new(bytes.Buffer)
-			tt.printType(writer, "Hello, world")
+			printer := convert.WithPrinter(tt.printer.Print)
+			p := convert.NewPrinter(printer)
+			p.Print(writer, "Hello, world")
 			got := tt.function(writer)
 
 			if got != tt.want {
